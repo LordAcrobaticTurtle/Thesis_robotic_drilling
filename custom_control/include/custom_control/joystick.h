45dc834b844edfb9f9b5859273d473d293947a66
sam <z5160890@ad.unsw.edu.au>
2022-09-30 16:26:27 +1000

    Skeleton for joystick class in c++

---
+++ custom_control/include/custom_control/joystick.h
@@ -0,0 +1,14 @@
+#include <ros/ros.h>
+#include <sensor_msgs/Joy.h>
+
+
+namespace thesis {
+
+    class joystick {
+        public:
+
+        private:
+            sensor_msgs::Joy joyState;
+    }
+
+}
