4c408dd9f69d4f22931310e4826de150efea4d36
Sam <z5160890@ad.unsw.edu.au>
2022-10-22 19:12:02 -0700

    Converted bags to csv Updated install script

--- install.sh
+++ install.sh
@@ -17,14 +17,14 @@ echo Continuing with installation
 
 # Install ROS
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt install curl # if you haven't already installed curl
+sudo apt install curl -y # if you haven't already installed curl 
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install ros-melodic-desktop-full -y
 sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential -y
 sudo apt install python-rosdep -y
-sudo rosdep init
+sudo rosdep init -y
 rosdep update
 
 
@@ -35,7 +35,7 @@ sudo apt-get install ros-melodic-ros-control ros-melodic-ros-controllers -y
 sudo apt install ros-melodic-moveit -y 
 
 # Install moveit visual tools
-sudo apt-get install ros-kinetic-moveit-visual-tools -y
+sudo apt-get install ros-melodic-moveit-visual-tools -y
 
 
 echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
@@ -43,14 +43,19 @@ echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
-git clone -b calibration_devel https://github.com/fmauch/universal_robot.git src/fmauch_universal_robot
-git clone https://github.com/UniversalRobots/Universal_Robots_ROS_Driver.git src/Universal_Robots_ROS_Driver
+git clone -b calibration_devel https://github.com/fmauch/universal_robot.git fmauch_universal_robot
+git clone https://github.com/UniversalRobots/Universal_Robots_ROS_Driver.git Universal_Robots_ROS_Driver
 git clone https://github.com/ros-planning/moveit_tutorials.git -b melodic-devel
 git clone https://github.com/ros-planning/panda_moveit_config.git -b melodic-devel
+git clone git@github.com:fzi-forschungszentrum-informatik/cartesian_controllers.git
+
 
 rosdep install -y --from-paths . --ignore-src --rosdistro melodic
 
-sudo apt-get install ros-melodic-moveit-visual-tools
+sudo apt-get install ros-melodic-moveit-visual-tools -y
+# sudo apt-get install ros-melodic-rqt-joint-trajectory-controller
+sudo apt-get install ros-melodic-rqt-controller-manager -y
+
 sudo apt install snapd
 sudo snap install code --classic
 echo 'export PATH=$PATH:/snap/bin' >> ~/.bashrc
