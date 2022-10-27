2c1ec43ddcfcd40a5a6c5c1b57cfa7a400084cc0
Sam <z5160890@ad.unsw.edu.au>
2022-10-26 20:29:31 -0700

    Removed weird import

--- custom_control/scripts/JoystickToWrench.py
+++ custom_control/scripts/JoystickToWrench.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-from asyncio.proactor_events import _ProactorSocketTransport
 import rospy
 from geometry_msgs.msg import WrenchStamped
 from geometry_msgs.msg import PoseStamped
